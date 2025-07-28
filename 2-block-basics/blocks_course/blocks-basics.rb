ad5332ba92dac10aa32ddca44edb9dd3e2a05bd7
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-28 12:34:03 -0600

    Add Block Basics blocks_course

--- 2-block-basics/blocks_course/blocks-basics.rb
+++ 2-block-basics/blocks_course/blocks-basics.rb
@@ -1,6 +1,34 @@
-5.times do |number|
-  puts "#{number} situp"
-  puts "#{number} pushup"
-  puts "#{number} chinup"
-  puts ""
+# 5.times do |number|
+#   puts "#{number} situp"
+#   puts "#{number} pushup"
+#   puts "#{number} chinup"
+#   puts ""
+# end
+
+# 3.times do |number|
+#   name = "Moe"
+#   puts "#{number} Howdy #{name}"
+# end
+# puts name
+
+# name = "Curly"
+# 3.times do |number|
+#   name = "Moe"
+#   puts "#{number} Howdy #{name}"
+# end
+# puts name
+
+# number = 100
+# 3.times do |number|
+#   name = "Moe"
+#   puts "#{number} Howdy #{name}"
+# end
+# puts number
+
+temp = Math::PI / 4
+3.times do |number; temp|
+  name = "Moe"
+  temp = 98.6
+  puts "#{number} Howdy #{name} is #{temp}"
 end
+puts temp
