f7ed5d258fbab9455105af47409b295aaf9e5002
spjesugq <jesus.gonzalez@simplepractice.com>
2025-07-30 09:47:47 -0600

    Add Yielding blocks_course

---
+++ 5-yielding/blocks_course/deal.rb
@@ -0,0 +1,43 @@
+def deal
+  faces = ["Jack", "Queen", "King", "Ace"]
+  suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+  if block_given?
+    random_face = faces.sample
+    random_suit = suits.sample
+    score = yield random_face, random_suit
+    puts "You scored a #{score}!"
+  else
+    puts "No deal!"
+  end
+end
+
+deal
+
+deal do |face, suit|
+  puts "Dealt a #{face} of #{suit}"
+  face.length + suit.length
+end
+
+puts ""
+
+def deal(number_of_cards)
+  faces = ["Jack", "Queen", "King", "Ace"]
+  suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+  if block_given?
+    1.upto(number_of_cards) do
+      random_face = faces.sample
+      random_suit = suits.sample
+      score = yield random_face, random_suit
+      puts "You scored a #{score}!"
+    end
+  else
+    puts "No deal!"
+  end
+end
+
+deal(2)
+
+deal(2) do |face, suit|
+  puts "Dealt a #{face} of #{suit}"
+  face.length + suit.length
+end
---
+++ 5-yielding/blocks_course/iterator.rb
@@ -0,0 +1,57 @@
+def three_times
+  yield
+  yield
+  yield
+end
+
+three_times { puts "Ho!" }
+
+puts ""
+
+def three_times
+  1.upto(3) do |count|
+    yield count
+  end
+end
+
+three_times do |number|
+  puts "#{number} situp"
+  puts "#{number} pushup"
+  puts "#{number} chinup"
+end
+
+puts ""
+
+def n_times(count)
+  1.upto(count) { |count| yield count }
+end
+
+n_times(5) do |n|
+  puts "#{n} situps"
+  puts "#{n} pushups"
+  puts "#{n} chinups"
+end
+
+puts ""
+
+def progress
+  0.step(100, 10) do |number|
+    yield number
+  end
+end
+
+progress { |percent| puts percent }
+
+puts ""
+
+def greet
+  yield "Larry", 18
+end
+
+greet { |name, age| puts "Hello, #{name}. You don't look #{age}!" }
+
+def greet
+  yield
+end
+
+greet { |name, age| puts "Hello, #{name}. You don't look #{age}!" }
